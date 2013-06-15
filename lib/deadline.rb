b08c05f0ff146bbbc48226dd9d91e6ca173f8a21
kokubun <takashi_kokubun@tekunodo.jp>
2013-06-15 16:24:06 +0900

    Implement commands

--- lib/deadline.rb
+++ lib/deadline.rb
@@ -1,5 +1,21 @@
 require "deadline/version"
 
 module Deadline
-  # Your code goes here...
+  def self.setup
+    case ARGV[0]
+    when "add"
+      puts "pending"
+    when "tasks"
+      puts "pending"
+    when "track"
+      puts "pending"
+    else
+      print(<<-"EOS")
+      Usage:
+        deadline add TASK_NAME DEADLINE
+        deadline tasks
+        deadline track
+      EOS
+    end
+  end
 end
--- spec/deadline/task_spec.rb
+++ spec/deadline/task_spec.rb
@@ -1,1 +1,7 @@
 require 'spec_helper'
+
+module Spellcard
+  describe Task do
+    ;
+  end
+end
