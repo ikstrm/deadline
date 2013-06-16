a6cf32558772033a89d1243fbeda96c319f276c9
kokubun <kokubun.t.aa@m.titech.ac.jp>
2013-06-16 19:48:25 +0900

    Change add command

--- lib/deadline.rb
+++ lib/deadline.rb
@@ -6,7 +6,11 @@ module Deadline
   def self.setup
     case ARGV[0]
     when "add"
-      puts "pending"
+      if ARGV.size < 3
+        puts "Usage: deadline add TASK_NAME DEADLINE"
+        return
+      end
+      Task.add(task: ARGV[1], deadline: ARGV[2])
     when "remove"
       puts "pending"
     when "tasks"
