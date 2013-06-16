aad52489250fec2a34323af6fdc5758b7baef4d3
kokubun <takashi_kokubun@tekunodo.jp>
2013-06-16 22:26:56 +0900

    Add spec for Task.refresh

--- spec/deadline/task_spec.rb
+++ spec/deadline/task_spec.rb
@@ -101,5 +101,49 @@ module Deadline
         end
       end
     end
+
+    describe ".refresh" do
+      describe "given disordered tasks" do
+        before do
+          Task.remove("all")
+          Task.add(task: "test1", deadline: "10")
+          Task.add(task: "test3", deadline: "30")
+          Task.add(task: "test2", deadline: "20")
+        end
+
+        it "should sort the tasks" do
+          Task.refresh
+          tasks = Task.all
+          tasks[0][:task].should == "test1"
+          tasks[1][:task].should == "test2"
+          tasks[2][:task].should == "test3"
+        end
+      end
+
+      describe "given an expired task" do
+        before do
+          Task.remove("all")
+
+          path = File.expand_path('~/.deadline/')
+          if FileTest.exist?(path) == false
+            FileUtils.mkdir_p(path)
+          end
+
+          File.open(File.expand_path('~/.deadline/tasks.yml'), 'w') do |f|
+            f << { tasks: [{
+                  task: "test",
+                  deadline: Time.local(2000,1,1,10,0,0)
+                }] }.to_yaml
+          end
+        end
+
+        it "should remove the task" do
+          tasks = Task.all
+          tasks.size.should == 0
+        end
+
+        it "should push growl notification"
+      end
+    end
   end
 end
